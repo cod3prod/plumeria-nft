8abecf200435719fd1ceb0c3a5969bc5b43886e8
cod3prod <cod3prod@gmail.com>
2025-02-09 00:04:40 +0900

    ✅ Update Test

--- test/test.ts
+++ test/test.ts
@@ -22,6 +22,51 @@ describe("Plumeria", function () {
     return { plumeria, publisher, user };
   }
 
+  it("Set Publisher 1", async function () {
+    const { plumeria, publisher, user } = await loadFixture(deployTokenFixture);
+
+    const tx = await plumeria.connect(publisher).setPublisher(user.address);
+    const receipt = await tx.wait();
+    console.log(
+      "Set Publisher event : ",
+      receipt?.logs[receipt?.logs.length - 1]
+    );
+    expect(await plumeria.publisher()).to.deep.equal(user.address);
+  });
+
+  it("Set Publisher 2", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+
+    await expect(
+      plumeria.connect(user).setPublisher(user.address)
+    ).to.be.revertedWith("You are not the publisher");
+  });
+
+  it("Mint", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    for (let i = 0; i < 16; i++) {
+      await plumeria.connect(user).mint(i + 1, 1);
+      expect(await plumeria.totalSupply(i + 1)).to.deep.equal(1);
+    }
+  });
+
+  it("MintBatch", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    const tokenIds = [];
+    const amounts = [];
+
+    for (let i = 0; i < 16; i++) {
+      tokenIds.push(i + 1);
+      amounts.push(1);
+    }
+
+    await plumeria.connect(user).mintBatch(tokenIds, amounts);
+
+    for (let i = 0; i < 16; i++) {
+      expect(await plumeria.totalSupply(i + 1)).to.deep.equal(1);
+    }
+  });
+
   it("Token upgrade", async function () {
     const { plumeria, user } = await loadFixture(deployTokenFixture);
 
@@ -29,17 +74,59 @@ describe("Plumeria", function () {
       await plumeria.connect(user).mint(i, 1);
     }
     console.log("before balances : ", await plumeria.getBalances(user.address));
+    expect(await plumeria.totalSupply(1)).to.deep.equal(1);
 
     await plumeria.connect(user).setApprovalForAll(plumeria.target, true);
-    await plumeria.connect(user).upgrade();
+    const tx = await plumeria.connect(user).upgrade();
+    const receipt = await tx.wait();
+    console.log(
+      "Token upgrade event : ",
+      receipt?.logs[receipt?.logs.length - 1]
+    );
 
     const after = await plumeria.getBalances(user.address);
     console.log("after balances : ", after);
 
     expect(after[0]).to.deep.equal(1);
+    expect(await plumeria.totalSupply(0)).to.deep.equal(1);
+    expect(await plumeria.totalSupply(1)).to.deep.equal(0);
   });
 
-  it("Token burn", async function () {
+  it("Burn Error", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    await expect(plumeria.connect(user).burn(1, 1)).to.be.reverted;
+  });
+
+  it("BurnBatch Error 1", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    await expect(plumeria.connect(user).burnBatch([1, 2], [1, 2])).to.be
+      .reverted;
+  });
+
+  it("BurnBatch Error 2", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    await plumeria.connect(user).mint(1, 1);
+    await expect(plumeria.connect(user).burnBatch([0, 1], [1, 1])).to.be
+      .reverted;
+  });
+
+  it("Burn Premium", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    for (let i = 1; i < 17; i++) {
+      await plumeria.connect(user).mint(i, 1);
+    }
+    await plumeria.connect(user).setApprovalForAll(plumeria.target, true);
+    const tx = await plumeria.connect(user).upgrade();
+    const receipt = await tx.wait();
+    console.log(
+      "Burn Premium event : ",
+      receipt?.logs[receipt?.logs.length - 1]
+    );
+    await plumeria.connect(user).burn(0, 1);
+    expect(await plumeria.totalSupply(0)).to.deep.equal(0);
+  });
+
+  it("Burn and BurnBatch", async function () {
     const { plumeria, user } = await loadFixture(deployTokenFixture);
     for (let i = 1; i < 17; i++) {
       await plumeria.connect(user).mint(i, 1);
@@ -58,10 +145,77 @@ describe("Plumeria", function () {
     ]);
   });
 
+  it("SEt URI 1", async function () {
+    const { plumeria, publisher } = await loadFixture(deployTokenFixture);
+    const tx = await plumeria.connect(publisher).setMetadataUri("TEST1/");
+    const receipt = await tx.wait();
+    console.log("Set URI event : ", receipt?.logs[receipt?.logs.length - 1]);
+    expect(await plumeria.uri(1)).to.equal("TEST1/1.json");
+  });
+
+  it("SEt URI 2", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+    await expect(
+      plumeria.connect(user).setMetadataUri("TEST2/")
+    ).to.be.revertedWith("You are not the publisher");
+  });
+
   it("Get URI", async function () {
     const { plumeria } = await loadFixture(deployTokenFixture);
     const uri = await plumeria.uri(1);
     console.log("uri : ", uri);
-    expect(uri).to.equal(METADATA_URI!);
+    expect(uri).to.equal(`${METADATA_URI!}1.json`);
+  });
+
+  it("Check Total Supply 1", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+
+    expect(await plumeria.totalSupply(0)).to.equal(0);
+
+    for (let i = 0; i < 500; i++) {
+      await plumeria.connect(user).mint(1, 10);
+    }
+
+    await expect(plumeria.connect(user).mint(1, 1)).to.be.revertedWith(
+      "The supply is full"
+    );
+  });
+
+  it("Check Total Supply 2", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+
+    expect(await plumeria.totalSupply(0)).to.equal(0);
+
+    for (let i = 0; i < 500; i++) {
+      await plumeria.connect(user).mint(16, 10);
+    }
+
+    const tokenIds = [];
+    const amounts = [];
+
+    for (let i = 0; i < 16; i++) {
+      tokenIds.push(i + 1);
+      amounts.push(1);
+    }
+
+    await expect(
+      plumeria.connect(user).mintBatch(tokenIds, amounts)
+    ).to.be.revertedWith("The supply is full");
+  });
+
+  it("Invalid mint", async function () {
+    const { plumeria, user } = await loadFixture(deployTokenFixture);
+
+    await expect(plumeria.connect(user).mint(0, 1)).to.be.revertedWith(
+      "Invalid token ID"
+    );
+
+    await expect(plumeria.connect(user).mint(17, 1)).to.be.revertedWith(
+      "Invalid token ID"
+    );
+
+    await expect(plumeria.connect(user).mint(16, 11)).to.be.revertedWith(
+      "Invalid amount"
+    );
   });
 });
